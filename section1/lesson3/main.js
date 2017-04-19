d2e8bad2fd408369e83b3344a42fdb7783d7dfb8
Rafael Rego <crafards@gmail.com>
2017-04-19 15:39:05 -0300

    add lesson 1 section 3

--- section1/lesson3/main.js
+++ section1/lesson3/main.js
@@ -1,3 +1,17 @@
 var app = angular.module('minmax', []);
 
 // https://minmax-server.herokuapp.com/register/'
+
+app.controller('MinMaxCtrl', function ($scope, $http) {
+    $scope.formData = {};
+    $scope.onSubmit = function () {
+        console.log("Submitted!");
+        $http.post("https://minmax-server.herokuapp.com/register/'", $scope.formData).
+            success(function(data){
+                console.log(":)");
+                console.log($scope.formData);
+            }).error(function (data) {
+                console.log(":(");
+        });
+    }
+});
