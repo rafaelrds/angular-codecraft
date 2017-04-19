6bc39f56b3095a56c4e4b0d07938e95598d8a875
Rafael Rego <crafards@gmail.com>
2017-04-19 17:45:36 -0300

    angular-auto-validate

--- section1/libs/angular-auto-validate/src/config/ngSubmitDecorator.js
+++ section1/libs/angular-auto-validate/src/config/ngSubmitDecorator.js
@@ -1,32 +1,78 @@
-(function (angular) {
-    'use strict';
+function SubmitDecorator($delegate, $parse, validationManager) {
+  $delegate[0].compile = function ($element, attrs) {
+    var fn = $parse(attrs.ngSubmit),
+      force = attrs.ngSubmitForce === 'true';
 
-    angular.module('jcs-autoValidate').config(['$provide',
-        function ($provide) {
-            $provide.decorator('ngSubmitDirective', [
-                '$delegate',
-                '$parse',
-                'validationManager',
-                function ($delegate, $parse, validationManager) {
-                    $delegate[0].compile = function ($element, attr) {
-                        var fn = $parse(attr.ngSubmit),
-                            force = attr.ngSubmitForce === 'true';
-                        return function (scope, element) {
-                            element.on('submit', function (event) {
-                                scope.$apply(function () {
-                                    if (validationManager.validateForm(element) || force === true) {
-                                        fn(scope, {
-                                            $event: event
-                                        });
-                                    }
-                                });
-                            });
-                        };
-                    };
+    return function (scope, element) {
+      var formController = element.controller('form'),
+        resetListenerOffFn;
 
-                    return $delegate;
-                }
-            ]);
+      function handlerFn(event) {
+        scope.$apply(function () {
+          if (formController !== undefined &&
+            formController !== null &&
+            formController.autoValidateFormOptions &&
+            formController.autoValidateFormOptions.disabled === true) {
+            fn(scope, {
+              $event: event
+            });
+          } else {
+            if (formController.$setSubmitted === undefined) {
+              // we probably have angular <= 1.2
+              formController.$submitted = true;
+            }
+
+            if (validationManager.validateForm(element) || force === true) {
+              fn(scope, {
+                $event: event
+              });
+            }
+          }
+        });
+      }
+
+      function resetFormFn() {
+        if (element[0].reset) {
+          element[0].reset();
+        } else {
+          validationManager.resetForm(element);
         }
-    ]);
-}(angular));
+      }
+
+      if (formController && formController.autoValidateFormOptions) {
+        // allow the form to be reset programatically or via raising the event
+        // form:formName:reset
+        formController.autoValidateFormOptions.resetForm = resetFormFn;
+        if (formController.$name !== undefined && formController.$name !== '') {
+          resetListenerOffFn = scope.$on('form:' + formController.$name + ':reset', resetFormFn);
+        }
+      }
+
+      element.on('submit', handlerFn);
+      scope.$on('$destroy', function () {
+        element.off('submit', handlerFn);
+        if (resetListenerOffFn) {
+          resetListenerOffFn();
+        }
+      });
+    };
+  };
+
+  return $delegate;
+}
+
+SubmitDecorator.$inject = [
+  '$delegate',
+  '$parse',
+  'validationManager'
+];
+
+function ProviderFn($provide) {
+  $provide.decorator('ngSubmitDirective', SubmitDecorator);
+}
+
+ProviderFn.$inject = [
+  '$provide'
+];
+
+angular.module('jcs-autoValidate').config(ProviderFn);
