68b65dd6aa8beefa91f7aa04799fffb4a3257a48
Rafael Rego <crafards@gmail.com>
2017-04-20 16:09:15 -0300

    fix section1 lesson5

--- section1/lesson5/main.js
+++ section1/lesson5/main.js
@@ -1,18 +1,26 @@
 var app = angular.module('minmax', []);
 
 
+
+
 app.controller('MinMaxCtrl', function ($scope, $http) {
-	$scope.formModel = {};
+    $scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+    $scope.onSubmit = function (valid) {
 
-		//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-		//	success(function (data) {
-		//		console.log(":)")
-		//	}).error(function(data) {
-		//		console.log(":(")
-		//	});
-	};
+        if (valid) {
+            console.log("Hey i'm submitted!");
+            console.log($scope.formModel);
+
+            //$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+            //	success(function (data) {
+            //		console.log(":)")
+            //	}).error(function(data) {
+            //		console.log(":(")
+            //	});
+        } else {
+            console.log("Invalid Form!")
+        }
+
+    };
 });
