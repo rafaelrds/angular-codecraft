9a9e1fb7760526a0b48251e3b34b3672be1dff5a
Rafael Rego <crafards@gmail.com>
2017-04-26 16:44:02 -0300

    section 4 lesson 2 completed. understanding of .

--- section4/lesson1/main.js
+++ section4/lesson1/main.js
@@ -7,7 +7,7 @@ app.controller('ParentController', function ($scope, $rootScope) {
 
 app.controller('ChildController', function ($scope, $rootScope) {
     $scope.reset = function () {
-        $rootScope.name = "Parent";
+        $rootScope.name = "Reset By Child";
     };
 });
 
