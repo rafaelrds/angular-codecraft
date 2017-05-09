e7872536b93574736360b492fb119de93ef4c754
Rafael <crafards@gmail.com>
2017-05-09 17:34:20 -0300

    search and order through api

--- section6/lesson1/main.js
+++ section6/lesson1/main.js
@@ -22,12 +22,19 @@ app.controller("PersonListController", function ($scope, ContactService) {
     $scope.order = "email";
     $scope.contacts = ContactService;
     $scope.loadMore = function () {
-        console.log("is loading");
         $scope.contacts.loadMore();
     };
     
     $scope.$watch("search", function (newVal, oldVal) {
-        console.log(newVal);
+        if (angular.isDefined(newVal)) {
+            $scope.contacts.doSearch(newVal);
+        }
+    });
+
+    $scope.$watch("order", function (newVal, oldVal) {
+        if (angular.isDefined(newVal)) {
+            $scope.contacts.doOrder(newVal);
+        }
     });
 
 });
@@ -42,12 +49,29 @@ app.service("ContactService", function (Contact) {
         "isLoading": false,
         "selectedPerson": null,
         "persons": [],
+        "resetVariables" : function () {
+            self.hasMore = true;
+            self.page = 1;
+            self.persons = [];
+        },
+        "doSearch" : function (search) {
+            self.resetVariables();
+            self.search = search;
+            self.loadContacts();
+        },
+        "doOrder" : function (order) {
+            self.resetVariables();
+            self.ordering = order;
+            self.loadContacts();
+        },
         "loadContacts": function () {
             if (self.hasMore && !self.isLoading) {
                 self.isLoading = true;
 
                 var params = {
-                  "page" : self.page
+                  "page" : self.page,
+                  "search" : self.search,
+                  "ordering" : self.ordering
                 };
 
                 Contact.get(params, function (data) {
