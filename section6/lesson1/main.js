cda8cf4cf9e52b2487e647e7a5b30ab6f2918f73
Rafael <crafards@gmail.com>
2017-05-09 16:25:53 -0300

    add infinite scrolling for calling api

--- section6/lesson1/main.js
+++ section6/lesson1/main.js
@@ -1,56 +1,79 @@
 var app = angular.module("codecraft", [
-	"ngResource"
+    "ngResource",
+    "infinite-scroll"
 ]);
 
 app.config(function ($httpProvider, $resourceProvider) {
-	$httpProvider.defaults.headers.common["Authorization"] = "Token " + config.my_key;
-	$resourceProvider.defaults.stripTrailingSlashes = false;
+    $httpProvider.defaults.headers.common["Authorization"] = "Token " + config.my_key;
+    $resourceProvider.defaults.stripTrailingSlashes = false;
 });
 
 app.factory("Contact", function ($resource) {
-	return $resource("https://api.codecraft.tv/samples/v1/contact/:id/");
-})
+    return $resource("https://api.codecraft.tv/samples/v1/contact/:id/");
+});
 
 app.controller("PersonDetailController", function ($scope, ContactService) {
-	$scope.contacts = ContactService;
+    $scope.contacts = ContactService;
 });
 
 app.controller("PersonListController", function ($scope, ContactService) {
 
-	$scope.search = "";
-	$scope.order = "email";
-	$scope.contacts = ContactService;
+    $scope.search = "";
+    $scope.order = "email";
+    $scope.contacts = ContactService;
+    $scope.loadMore = function () {
+        console.log("is loading");
+        $scope.contacts.loadMore();
+    };
 
-	$scope.sensitiveSearch = function (person) {
-		if ($scope.search) {
-			return person.name.indexOf($scope.search) == 0 ||
-				person.email.indexOf($scope.search) == 0;
-		}
-		return true;
-	};
+    $scope.sensitiveSearch = function (person) {
+        if ($scope.search) {
+            return person.name.indexOf($scope.search) == 0 ||
+                person.email.indexOf($scope.search) == 0;
+        }
+        return true;
+    };
 
 });
 
 app.service("ContactService", function (Contact) {
-	var self = {
-		"addPerson": function (person) {
-			this.persons.push(person);
-		},
-		"page" : 1,
-		"hasMore" : true,
-		"isLoading" : false,
-		"selectedPerson": null,
-		"persons": [],
-		"loadContacts" : function () {
-            Contact.get(function (data) {
-                console.log(data);
-                angular.forEach(data.results, function (person) {
-					self.persons.push(new Contact(person));
-                });
-            });
-        }
-	};
-	self.loadContacts();
+    var self = {
+        "addPerson": function (person) {
+            this.persons.push(person);
+        },
+        "page": 1,
+        "hasMore": true,
+        "isLoading": false,
+        "selectedPerson": null,
+        "persons": [],
+        "loadContacts": function () {
+            if (self.hasMore && !self.isLoading) {
+                self.isLoading = true;
 
-	return self;
+                var params = {
+                  "page" : self.page
+                };
+
+                Contact.get(params, function (data) {
+                    console.log(data);
+                    angular.forEach(data.results, function (person) {
+                        self.persons.push(new Contact(person));
+                    });
+                    if (!data.next) {
+                        self.hasMore = false;
+                    }
+                    self.isLoading = false;
+                });
+            }
+        },
+        "loadMore" : function () {
+            if (self.hasMore && !self.isLoading) {
+                self.page += 1;
+                self.loadContacts();
+            }
+        }
+    };
+    self.loadContacts();
+
+    return self;
 });
